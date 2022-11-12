0684b2fdaacee9389fe69cb2aa3b2c93c7179714
shagundalal <dalal.shagun@gmail.com>
2022-11-12 15:25:12 -0600

    local changes

--- customassure-app/src/App.js
+++ customassure-app/src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
+import {createTheme, ThemeProvider} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {CustomBtn} from './components/CustomBtn'
+
 import './App.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "5rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+  grid:{
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap", 
+  },
+})
+
+
 function App() {
+  const classes = styles(); 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <CustomBtn/>
+      </ThemeProvider>
     </div>
   );
+
 }
 
 export default App;
---
+++ customassure-app/src/components/CustomBtn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button} from '@material-ui/core';
+
+function CustomBtn() {
+    return (
+        <Button> Hiiiii </Button>
+    )
+}
+
+export default CustomBtn
