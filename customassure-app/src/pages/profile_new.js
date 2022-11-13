cef1a6e79c5894c74c4fcac3ed0546512ce66645
Aaron Tran <48807599+aaronctran@users.noreply.github.com>
2022-11-12 23:22:47 -0600

    Profile

--- customassure-app/src/pages/profile_new.js
+++ customassure-app/src/pages/profile_new.js
@@ -8,13 +8,6 @@ import {
   MDBCardBody,
   MDBCardImage,
   MDBBtn,
-  MDBBreadcrumb,
-  MDBBreadcrumbItem,
-  MDBProgress,
-  MDBProgressBar,
-  MDBIcon,
-  MDBListGroup,
-  MDBListGroupItem
 } from 'mdb-react-ui-kit';
 import './profile_new.css';
 import Plot from 'react-plotly.js';
